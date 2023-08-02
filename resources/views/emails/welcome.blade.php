10832711e2841ad18f10b6f3f5f41a32ea8bcc19
Felipe Mourão <felipemourao6124@gmail.com>
2023-08-01 23:59:27 -0300

    verifyemail

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -1,2 +1,3 @@
 <p>Olá {{ $user->first_name }}</p>
 <p>Seja bem vindo ao primeira api feita por Felipe Mourão!</p>
+<p>{{ $user->token}}</p>
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,12 +9,11 @@ use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Me\MeController;
+use App\Http\Controllers\Auth\VerifyEmailController;
 
 Route::middleware('auth:sanctum')->get('/me', [MeController::class, 'show']);
 
 Route::post('/login', LoginController::class);
 Route::post('/logout', LogoutController::class);
 Route::post('/register', RegisterController::class);
-
-
-
+Route::post('/verify-email', VerifyEmailController::class);
